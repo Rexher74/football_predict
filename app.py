78970be2ce8fa71b6813d7a65d1a45f4feaeb41f
Rexher74 <79226670+Rexher74@users.noreply.github.com>
2024-03-13 23:24:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import requests
 from bs4 import BeautifulSoup
 from flask_mysqldb import MySQL
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
-app.secret_key = "supersecretkey"
+app.secret_key = os.getenv('SECRET_KEY')
 
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "piticolon"
-app.config["MYSQL_DB"] = "footballprediction"
+app.config["MYSQL_HOST"] = os.getenv('MYSQL_HOST')
+app.config["MYSQL_USER"] = os.getenv('MYSQL_USER')
+app.config["MYSQL_PASSWORD"] = os.getenv('MYSQL_PASSWORD')
+app.config["MYSQL_DB"] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
